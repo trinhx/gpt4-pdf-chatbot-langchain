ae40fe6fee32d2fccf2dc0028117a0d48b0ea1fc
Tony Trinh <trinhx@gmail.com>
2023-04-12 09:09:22 +1000

    updated prompts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -4,6 +4,14 @@ import { PineconeStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
+// const CONDENSE_PROMPT =
+//   PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+//
+// Chat History:
+// {chat_history}
+// Follow Up Input: {question}
+// Standalone question:`);
+
 const CONDENSE_PROMPT =
   PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
@@ -13,9 +21,9 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
+  `You are an empathetic therapist providing helpful advice and wisdom. Be personable and colloquial in your response. Provide examples in your response pointing to the context provided.
 You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
-If you can't find the answer in the context below, just say "Hmm, I'm not sure." Don't try to make up an answer.
+If you can't find the answer in the context below, just say "I'm sorry I do not know the answer to that." Don't try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 Question: {question}
